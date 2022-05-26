2434ea1bf1fb5d300a44207fe9321fd8def6a5ed
yap-yap-yap <zhihanyap@gmail.com>
2022-05-26 14:44:08 +0800

    Add hw-w2 base files

---
+++ hw-w2/word_ladder.py
@@ -0,0 +1,15 @@
+from search_py3.search import Problem
+from search_py3.utils import *
+
+WORDS = set(i.lower().strip() for i in open('words2.txt'))
+def is_valid_word(word):
+    return word in WORDS
+
+class WordLadder(Problem):
+    def actions():
+        pass
+    def result():
+        pass
+    def result():
+        pass
+
