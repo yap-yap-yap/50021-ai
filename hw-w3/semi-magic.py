558b7b5f842b19b1e124b4883f5ebfbe40c867f0
yap-yap-yap <zhihanyap@gmail.com>
2022-06-09 16:35:30 +0800

    Add completed solve_semi_magic CSP

--- hw-w3/semi-magic.py
+++ hw-w3/semi-magic.py
@@ -17,23 +17,55 @@ def solve_semi_magic(algorithm=backtracking_search, **args):
     #########################################
     # Fill in these definitions
 
-    csp_domains = None
-    csp_neighbors = None
-    def csp_constraints(A, a, B, b):
-        pass
+    csp_domains = {
+        'V1': [1, 2, 3],
+        'V2': [1, 2, 3],
+        'V3': [1, 2, 3],
+        'V4': [1, 2, 3],
+        'V5': [1, 2, 3],
+        'V6': [1, 2, 3],
+        'V7': [1, 2, 3],
+        'V8': [1, 2, 3],
+        'V9': [1, 2, 3]
+    }
 
+    # each pair of neighbours make up the row/column/diagonal
+    csp_neighbors = {
+        'V1': ['V2', 'V3', 'V4', 'V7', 'V5', 'V9'],
+        'V2': ['V1', 'V3', 'V5', 'V8'],
+        'V3': ['V1', 'V2', 'V6', 'V9'],
+        'V4': ['V1', 'V7', 'V5', 'V6'],
+        'V5': ['V2', 'V8', 'V4', 'V6', 'V1', 'V9'],
+        'V6': ['V3', 'V9', 'V4', 'V5'],
+        'V7': ['V1', 'V4', 'V8', 'V9'],
+        'V8': ['V2', 'V5', 'V7', 'V9'],
+        'V9': ['V3', 'V6', 'V7', 'V8', 'V1', 'V5']
+    }
+
+    def csp_constraints(A, a, B, b):
+        if a == b:
+            return False
+        return True
+        
     #########################################
     
     # define the CSP instance
     csp = CSP(csp_vars, csp_domains, csp_neighbors,
-              csp_constraint)
+              csp_constraints)
 
     # run the specified algorithm to get an answer (or None)
     ans = algorithm(csp, **args)
     
-    print 'number of assignments', csp.nassigns
+    print('number of assignments', csp.nassigns)
     assign = csp.infer_assignment()
     if assign:
         for x in sorted(assign.items()):
-            print x
+            print(x)
     return csp
+
+
+def main():
+    solve_semi_magic()
+
+if __name__ == "__main__":
+    main()
